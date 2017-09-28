f5cafc5f31594854bf75332573c980c3c2de79fd
James Martinez <jamescmartinez@gmail.com>
2017-09-27 18:49:33 -0700

    Use erb for index view

--- app.rb
+++ app.rb
@@ -5,9 +5,10 @@ require './services/calendar_service.rb'
 get '/' do
   event = CalendarService.new.next_event
   datetime = event.datetime.in_time_zone('US/Pacific')
-  date = datetime.to_date.to_formatted_s(:long)
-  time = datetime.strftime('%-I:%M%p')
-  "The next iOS coffee will be at #{event.location} at #{time} on #{date}."
+  @date = datetime.to_date.to_formatted_s(:long)
+  @time = datetime.strftime('%-I:%M%p')
+  @location = event.location
+  erb :index
 end
 
 get '/api/events' do
