6eb94b4ea7fc4dc2246c52e001cf66ded1616256
brennanMKE <brennan@smallsharptools.com>
2017-10-04 09:27:15 -0700

    fixes bugs and adds refinements

--- static/coffee.js
+++ static/coffee.js
@@ -4,37 +4,55 @@
     let version = "20170801"
     let near = "San Francisco, CA";
 
+    let debug = false;
+
+    function log() {
+        if (debug) {
+            console.log.apply(this, arguments);
+        }
+    }
+
     function changeImageURL(imageURL) {
+        log(["changing image URL", imageURL]);
         $('#image').attr("src", imageURL);
     }
 
-    function setDefaultImage() {
+    function showDefaultImage() {
+        log("setting default image");
         changeImageURL("coffee-bean.png");
     }
 
-    function fetchVenue(id) {
-        let venueURL = "https://api.foursquare.com/v2/venues/" + id;
+    function changeLink(venue) {
+        if (venue && venue.id) {
+            let venueURL = "https://foursquare.com/v/" + venue.id;
+            log(["changing link", venueURL]);
+            $("#link").attr("href", venueURL);
+        }
+    }
+
+    function fetchVenue(venue) {
+        log(["fetching venue", venue]);
+        let venueURL = "https://api.foursquare.com/v2/venues/" + venue.id;
         $.getJSON(venueURL, {
                 client_id: client_id,
                 client_secret: client_secret,
                 v: version
             })
             .done(function(json) {
-                console.log("venue");
-                console.log(json);
+                log(["venue", json]);
                 if (json.response && json.response.venue && json.response.venue.bestPhoto) {
                     let bestPhoto = json.response.venue.bestPhoto;
                     let imageURL = bestPhoto.prefix + "300x300" + bestPhoto.suffix;
                     changeImageURL(imageURL);
                 } else {
-                    setDefaultImage();
+                    showDefaultImage();
                 }
             });
     }
 
     function runVenueSearch(location) {
         // remove city and state from the location string
-        let query = location.replace(/\,\s\w+\, \w\w$/i, "");
+        let query = location.replace(/\,\s[\w ]+\, \w\w$/i, "");
         let searchURL = "https://api.foursquare.com/v2/venues/search";
         $.getJSON(searchURL, {
                 client_id: client_id,
@@ -44,12 +62,18 @@
                 query: query
             })
             .done(function(json) {
-                console.log("venue search");
-                console.log(json);
-                if (json.response && json.response.venues && json.response.venues.length > 0) {
-                    fetchVenue(json.response.venues[0].id);
+                log(["venue search: " + query, json]);
+                if (json.response && json.response.venues) {
+                    if (json.response.venues.length > 0) {
+                        let venue = json.response.venues[0];
+                        fetchVenue(venue);
+                        changeLink(venue);
+                    } else {
+                        log("No search results");
+                        showDefaultImage();
+                    }
                 } else {
-                    setDefaultImage();
+                    showDefaultImage();
                 }
             });
     }
@@ -59,13 +83,12 @@
         let futureEventURL = "/api/events/future";
         $.getJSON(futureEventURL)
             .done(function(json) {
-                console.log("future");
-                console.log(json);
+                log(["future", json]);
                 if ($.isArray(json) && json.length > 0) {
                     let location = json[json.length - 1].location;
                     runVenueSearch(location);
                 } else {
-                    setDefaultImage();
+                    showDefaultImage();
                 }
             });
     }
