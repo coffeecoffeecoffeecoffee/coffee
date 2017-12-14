a9747cafbe4f67ed3699b69564de499ec39eeee9
James Martinez <jamescmartinez@gmail.com>
2017-12-13 21:03:12 -0800

    Add ability to change group emoji to admin

--- app/dashboards/group_dashboard.rb
+++ app/dashboards/group_dashboard.rb
@@ -6,24 +6,28 @@ class GroupDashboard < Administrate::BaseDashboard
     id: Field::String.with_options(searchable: false),
     name: Field::String,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime
+    updated_at: Field::DateTime,
+    emoji: Field::String
   }.freeze
 
   COLLECTION_ATTRIBUTES = %i[
     id
     name
+    emoji
     created_at
   ].freeze
 
   SHOW_PAGE_ATTRIBUTES = %i[
     id
     name
+    emoji
     created_at
     updated_at
   ].freeze
 
-  FORM_ATTRIBUTES = [
-    :name
+  FORM_ATTRIBUTES = %i[
+    name
+    emoji
   ].freeze
 
   def display_resource(group)
