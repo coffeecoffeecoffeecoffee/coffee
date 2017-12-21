771b7ddb8ff38ae43c8122aac548f5396b8975b7
James Martinez <jamescmartinez@gmail.com>
2017-12-20 19:00:58 -0800

    Fix calendar end time

--- app/services/group_calendar_creator.rb
+++ app/services/group_calendar_creator.rb
@@ -10,7 +10,7 @@ class GroupCalendarCreator
     events.each do |event|
       calendar_event = Icalendar::Event.new
       calendar_event.dtstart = event.start_at
-      calendar_event.dtend = event.start_at
+      calendar_event.dtend = event.end_at
       calendar_event.summary = "#{@group.name}: #{event.location}"
       calendar.add_event(calendar_event)
     end
--- spec/services/group_calendar_creator_spec.rb
+++ spec/services/group_calendar_creator_spec.rb
@@ -4,12 +4,17 @@ describe GroupCalendarCreator do
   describe '#to_ical' do
     it 'creates an ical with all events' do
       group = create(:group)
-      create_list(:future_event, 3, group: group)
+      events = create_list(:future_event, 3, group: group)
 
       ical = GroupCalendarCreator.new(group).to_ical
       calendar_events = Icalendar::Event.parse(ical)
 
       expect(calendar_events.count).to eq(3)
+
+      event = events.first
+      calendar_event = calendar_events.first
+      expect(calendar_event.dtstart.to_time.to_i).to eq(event.start_at.to_i)
+      expect(calendar_event.dtend.to_time.to_i).to eq(event.end_at.to_i)
     end
   end
 end
