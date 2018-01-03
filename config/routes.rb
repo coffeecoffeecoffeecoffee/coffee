1563f0aa1a793866ec652ed570eb9b77c47a732b
James Martinez <jamescmartinez@gmail.com>
2018-01-02 20:06:05 -0800

    Lower groups search in routes so admin/static pages are accessible

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root to: 'events#index'
 
-  resources :groups, path: '', only: :show do
-    get '/ical', to: 'calendars#show'
-  end
-
-  get '/groups/:id', to: redirect('/%{id}') # rubocop:disable Style/FormatStringToken
-
   namespace :admin do
     root to: 'events#index'
 
     resources :events
     resources :groups
+  end
+
+  get '/groups/:id', to: redirect('/%{id}') # rubocop:disable Style/FormatStringToken
+
+  resources :groups, path: '', only: :show do
+    get '/ical', to: 'calendars#show'
   end
 end
