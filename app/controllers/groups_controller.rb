0b21a95939f762e95001b040a10a3d2c6071b977
James Martinez <jamescmartinez@gmail.com>
2018-01-18 15:31:58 -0500

    Extract event list into partial for cleaner code

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,5 @@ class GroupsController < ApplicationController
   def show
     @group = Group.friendly.find(params[:id]).decorate
     @events = @group.events.future_or_now.decorate
-    render 'events/index'
   end
 end
