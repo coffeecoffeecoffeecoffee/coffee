03006e7de61d5cbae24df6b4b95968be59db1dd6
James Martinez <jamescmartinez@gmail.com>
2018-02-01 17:04:13 -0800

    Fix crash when creating new event

--- app/fields/event_time_field.rb
+++ app/fields/event_time_field.rb
@@ -2,6 +2,7 @@ require 'administrate/field/base'
 
 class EventTimeField < Administrate::Field::Base
   def to_s
+    return nil unless resource.group
     time_zone = resource.group.time_zone
     data.in_time_zone(time_zone)
   end
