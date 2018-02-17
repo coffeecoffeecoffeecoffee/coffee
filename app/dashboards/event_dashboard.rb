f1d61c3ab6b3fd8bfda89e27c894479c0f08836b
James Martinez <jamescmartinez@gmail.com>
2018-02-17 09:28:10 -0800

    Use PST for admin dashboard time zones

--- app/dashboards/event_dashboard.rb
+++ app/dashboards/event_dashboard.rb
@@ -3,11 +3,11 @@ require 'administrate/base_dashboard'
 class EventDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::String.with_options(searchable: false),
-    start_at: Field::DateTime,
-    end_at: Field::DateTime,
+    start_at: Field::DateTime.with_options(timezone: 'America/Los_Angeles'),
+    end_at: Field::DateTime.with_options(timezone: 'America/Los_Angeles'),
     location: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    created_at: Field::DateTime.with_options(timezone: 'America/Los_Angeles'),
+    updated_at: Field::DateTime.with_options(timezone: 'America/Los_Angeles'),
     group: Field::BelongsTo,
     venue_foursquare_id: Field::String
   }.freeze
--- app/dashboards/group_dashboard.rb
+++ app/dashboards/group_dashboard.rb
@@ -5,8 +5,8 @@ class GroupDashboard < Administrate::BaseDashboard
     events: Field::HasMany,
     id: Field::String.with_options(searchable: false),
     name: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    created_at: Field::DateTime.with_options(timezone: 'America/Los_Angeles'),
+    updated_at: Field::DateTime.with_options(timezone: 'America/Los_Angeles'),
     emoji: Field::String,
     time_zone: Field::String
   }.freeze
