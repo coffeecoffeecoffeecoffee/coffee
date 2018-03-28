229dd7a2d72d26ace897d7d2d48e1d7a719578cb
James Martinez <jamescmartinez@gmail.com>
2018-03-27 17:01:06 -0700

    Validate factories

---
+++ spec/factories_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+FactoryBot.factories.map(&:name).each do |factory|
+  describe "The #{factory} factory" do
+    it 'is valid' do
+      expect(build(factory)).to be_valid
+    end
+  end
+end
