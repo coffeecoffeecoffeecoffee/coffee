010a87f2a4b512afeb43112fea5199f76c947db8
James Martinez <jamescmartinez@gmail.com>
2018-06-05 20:59:58 -0700

    Disable Rails/BulkChangeTable for old migrations

--- db/migrate/20180112005559_add_venue_to_events.rb
+++ db/migrate/20180112005559_add_venue_to_events.rb
@@ -1,6 +1,8 @@
+# rubocop:disable Rails/BulkChangeTable
 class AddVenueToEvents < ActiveRecord::Migration[5.1]
   def change
     add_column :events, :venue_foursquare_id, :string, null: false, default: ''
     change_column_default :events, :venue_foursquare_id, from: '', to: nil
   end
 end
+# rubocop:enable Rails/BulkChangeTable
