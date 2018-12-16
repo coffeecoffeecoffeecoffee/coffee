61c9abe2740ca7409908ea32ff0146dc94ee7853
James Martinez <jamescmartinez@gmail.com>
2018-12-15 17:18:47 -0800

    Dangerously consolidate migrations to fix prod issues

--- db/migrate/20181215215423_create_venues.rb
+++ db/migrate/20181215215423_create_venues.rb
@@ -2,17 +2,24 @@ class CreateVenues < ActiveRecord::Migration[5.2]
   def change
     create_table :venues, id: :uuid do |t|
       t.string :foursquare_id, null: false
-      t.string :name, null: false
-      t.string :foursquare_url, null: false
-      t.string :image_url, null: false
+      t.string :name
+      t.string :foursquare_url
+      t.string :image_url
 
       t.timestamps
     end
 
     add_column :events, :venue_id, :uuid
 
-    venue = Venue.create!(foursquare_id: '', name: '', foursquare_url: '', image_url: '')
+    reversible do |direction|
+      direction.up do
+        Event.find_each do |e|
+          venue = Venue.create!(foursquare_id: e.venue_foursquare_id)
+          e.update_columns(venue_id: venue.id) # rubocop:disable Rails/SkipsModelValidations
+        end
+      end
+    end
 
-    change_column_null :events, :venue_id, false, venue.id
+    change_column_null :events, :venue_id, false
   end
 end
