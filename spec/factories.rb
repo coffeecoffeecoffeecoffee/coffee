137bf00ec8c7ac8fbe9cbc5417d538d2dfc9e73c
James Martinez <jamescmartinez@gmail.com>
2018-12-18 12:39:40 -0800

    Use dummy data for factories instead of importing VCR data

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,9 +6,21 @@ FactoryBot.define do
     group
     foursquare_venue_id { "4feddd79d86cd6f22dc171a9" }
     foursquare_venue_data do
-      response_string = VCR::Cassette.new(:foursquare_venue_details).http_interactions.interactions.first.response.body
-      body = JSON.parse(response_string, symbolize_names: true)
-      body[:response][:venue]
+      # This data is copied from the VCR cassette :foursquare_venue_details
+      # It only copies the data we use. There's tons of other data in there.
+      {
+        id: "4feddd79d86cd6f22dc171a9",
+        name: "The Mill", location: {
+          formattedAddress: ["736 Divisadero St (btwn Grove St & Fulton St)", "San Francisco, CA 94117", "United States"]
+        },
+        canonicalUrl: "https://foursquare.com/v/the-mill/4feddd79d86cd6f22dc171a9",
+        bestPhoto: {
+          prefix: "https://igx.4sqi.net/img/general/",
+          suffix: "/403777_tR60tUZMVoJ5Q5ylr8hQnp0pgZTy5BOQLqydzAoHWiA.jpg",
+          width: 612,
+          height: 612
+        }
+      }
     end
 
     factory :future_event do
