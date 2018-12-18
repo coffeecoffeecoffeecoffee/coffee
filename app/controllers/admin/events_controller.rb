dc9adec0d43c6f7c78fdc3ef5a254f634581902c
James Martinez <jamescmartinez@gmail.com>
2018-12-18 15:50:25 -0800

    Sort events in admin panel by start date by default

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,4 +1,16 @@
 module Admin
   class EventsController < Admin::ApplicationController
+    before_action :default_params
+
+    private
+
+    # Change default sort order of events
+    # https://github.com/thoughtbot/administrate/issues/442#issuecomment-439209910
+    def default_params
+      resource_params = params.fetch(resource_name, {})
+      order = resource_params.fetch(:order, "start_at")
+      direction = resource_params.fetch(:direction, "desc")
+      params[resource_name] = resource_params.merge(order: order, direction: direction)
+    end
   end
 end
