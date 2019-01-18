bdd6e79b88fa188bf4526dfa3d0d3356a0f81bf4
James Martinez <jamescmartinez@gmail.com>
2019-01-17 16:26:51 -0800

    Remove unused stuff

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -26,8 +26,5 @@ module Coffee
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
