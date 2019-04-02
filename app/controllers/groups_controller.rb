8a115e3e39753996c08cd365ce2d277378f152c0
Roderic Campbell <roderic@gmail.com>
2019-04-02 16:32:54 -0700

    Add meta tags for app store

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,5 +7,6 @@ class GroupsController < ApplicationController
     @group = Group.friendly.find(params[:id])
     @events = @group.events.future_or_now
     @past_events = @group.events.past
+    set_meta_tags "apple-itunes-app": "app-id=1458031604"
   end
 end
