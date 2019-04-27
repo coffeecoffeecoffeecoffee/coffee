dcf4b75115710194b0621050b2a5623cd5f81477
James Martinez <jamescmartinez@gmail.com>
2019-04-27 12:29:27 -0700

    Extract GraphQL queries from query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,5 @@
 module Types
   class QueryType < GraphQL::Schema::Object
-    field :groups, [GroupType], null: false do
-      argument :id, ID, required: false
-    end
-
-    def groups(id: nil)
-      return Group.all unless id
-
-      [Group.friendly.find(id)]
-    end
+    include Queries::GroupsQuery
   end
 end
