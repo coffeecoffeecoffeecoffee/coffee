a0f5d5664f27d05297e99221e6b834e2a284f1bf
James Martinez <jamescmartinez@gmail.com>
2019-04-27 21:45:23 -0700

    Allow event images to be nil in GraphQL

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -2,7 +2,7 @@ module Types
   class EventType < GraphQL::Schema::Object
     field :id, ID, null: false
     field :name, String, null: false
-    field :image_url, String, null: false
+    field :image_url, String, null: true
     field :start_at, GraphQL::Types::ISO8601DateTime, null: false
     field :end_at, GraphQL::Types::ISO8601DateTime, null: false
     field :venue, Types::VenueType, null: false
