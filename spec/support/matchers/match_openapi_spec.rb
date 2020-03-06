5dbd8de7470ca8e38b54f39e044cc252393b76ab
James Martinez <jamescmartinez@gmail.com>
2020-03-06 17:13:27 -0500

    Update Rails defaults to Rails 6 (#271)

--- spec/support/matchers/match_openapi_spec.rb
+++ spec/support/matchers/match_openapi_spec.rb
@@ -3,7 +3,7 @@ require "rspec/expectations"
 RSpec::Matchers.define :match_openapi_spec do |path, verb|
   match do |response|
     status = response.status.to_s
-    content_type = response.content_type
+    content_type = response.content_type.split("\;").first
     body = JSON.parse(response.body)
 
     @actual = body
@@ -11,8 +11,8 @@ RSpec::Matchers.define :match_openapi_spec do |path, verb|
     definition = OasParser::Definition.resolve("docs/openapi.yaml")
     path = definition.path_by_path(path)
     endpoint = path.endpoint_by_method(verb)
-    response = endpoint.response_by_code(status)
-    schema_data = response.schema(content_type).as_json
+    endpoint_response = endpoint.response_by_code(status)
+    schema_data = endpoint_response.schema(content_type).as_json
 
     @expected = schema_data
 
