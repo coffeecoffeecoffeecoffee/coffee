40669dc57e33187558576a540c0220a37e4e8950
James Martinez <jamescmartinez@gmail.com>
2021-06-09 10:28:01 -0500

    Remove OmniAuth on_failure block

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,
            Rails.application.credentials.twitter[:consumer_key],
            Rails.application.credentials.twitter[:consumer_secret]
-
-  OmniAuth.config.on_failure = proc { |env|
-    OmniAuth::FailureEndpoint.new(env).redirect_to_failure
-  }
 end
